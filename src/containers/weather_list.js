8c873fdbd0c9a1dc3cab3f59e30ad94f3b9719d8
JimiJonJimbo <jimmyling3@gmail.com>
2018-04-24 11:42:31 -0700

    Finished lecture 64

---
+++ src/containers/weather_list.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+// The below is equivalent to doing
+// mapStateToProps(state) followed by
+// const weather = state.weather;
+function mapStateToProps({ weather }) {
+  // The below is equivalent to doing
+  // return { weather: weather };
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,17 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+// The initial state is an array because we will have a list of
+// cities.
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+    // DON'T DO
+    // return state.push(action.payload.data);
+    // because you never manipulate state directly! Even in Redux.
+    // You return a completely new instance of state. So do this:
+    // return state.concat([action.payload.data]);
+    // OR you can do this:
+    return [ action.payload.data, ...state ];
+  }
   return state;
 }
